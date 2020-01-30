45d57ed4d431e72230c868401dd918b1b8f7c447
Chelsea Kramer <chelsea.kramer99@gmail.com>
2020-01-29 19:50:10 -0500

    resolving conflicts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from "react";
+import axios from 'axios'
+
+const API = 'https://acme-users-api-rev.herokuapp.com/api';
+
+const fetchUser = async ()=> {
+  const storage = window.localStorage;
+  const userId = storage.getItem('userId')
+  if(userId){
+    try {
+      return (await axios.get(`${API}/users/detail/${userId}`)).data
+    }
+    catch(ex){
+      storage.removeItem('userId')
+      return fetchUser()
+    }
+  }
+  const user = (await axios.get(`${API}/users/random`)).data
+  console.log(user)
+  storage.setItem('userId', user.id)
+  return  user
+};
 
 function App() {
-  return (
-    <div>
-      Hello partner B!
-    </div>
-  );
+
+	return (
+		<div>
+
+		</div>
+	);
 }
 
 export default App;
